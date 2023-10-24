7be806de3c807bfc37a2c235422bb254a0804657
ObiChika <amandachika85@gmail.com>
2023-10-24 14:35:30 -0700

    Python class and object

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""Define a class Square"""
+
+
+class Square:
+    """ defines a function named __init__ """
+
+    def __init__(self, size=0):
+        """ if statement """
+        if type(size) != int:
+            """ raise an error """
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            """ raise an error """
+            raise ValueError("size must be >= 0")
+        else:
+            """ initialize __size of self with size """
+            self.__size = size

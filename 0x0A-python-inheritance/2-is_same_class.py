eb89bf0bdf3dfacdbcf5d275bd882693c6192e18
ObiChika <amandachika85@gmail.com>
2023-11-06 14:34:38 -0800

    if the object is exactly an instance

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module has a functction that checks if an object is an instance
+  or a specified class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
+
+    Returns:
+        bool: True if the object is an instance, otherwise False.
+    """
+    return type(obj) == a_class

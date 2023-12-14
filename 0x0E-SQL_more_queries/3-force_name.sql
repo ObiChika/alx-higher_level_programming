738ea55459718187eb0e0b51e6cb6d640c864d06
ObiChika <amandachika85@gmail.com>
2023-12-14 03:03:56 -0800

    SQL more queries

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- Script to create the table force_name
+/* Using the specified database */
+/* Creating the table if not exists */
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256) NOT NULL
+);
